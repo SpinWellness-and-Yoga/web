86c47a28a36866279c44a41ec4d229295399be0a
Brainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2025-11-10 04:43:14 +0000

    fixed bbuild and deploy scripts

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import WaitlistCard from "./_components/WaitlistCard";
 import styles from "./page.module.css";
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: "Spinwellness & Yoga | Holistic Employee Wellness",
   description:
