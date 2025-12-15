197db42265597dc1b2884f1a2e60beb618678ded
Brainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2025-12-15 00:27:12 +0000

    added config for output file tracing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.resolve(__dirname),
 
   compress: true,
   
